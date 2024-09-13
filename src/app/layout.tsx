65631fa6576032e5bcf94360b214ef361a26a2e3
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 07:25:11 +0300

    Refactor RootLayout component to remove unnecessary background color class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0a0a0a] select-none`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  select-none`}
       >
         <ReactQueryClientProvider>
           {children}

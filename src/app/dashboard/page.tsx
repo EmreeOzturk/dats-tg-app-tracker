756a6524bd5464fad96df7a40f048a1ac32d71e3
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 07:25:16 +0300

    Refactor User Dashboard page layout and add UserTable component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,12 +8,16 @@ export default async function DashboardPage() {
         redirect("/");
     }
     return (
-        <div className="container mx-auto py-10">
-            <div className='flex items-center justify-between w-full'>
-                <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
-                <LogoutButton />
+        <div className="w-full relative overflow-hidden h-screen py-12 px-4
+          bg-gradient-to-b from-slate-950 to-slate-900
+        ">
+            <div className="max-w-7xl mx-auto w-full relative overflow-hidden  p-4  ">
+                <div className='flex items-center justify-between w-full'>
+                    <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
+                    <LogoutButton />
+                </div>
+                <UserTable />
             </div>
-            <UserTable />
         </div>
     );
 }

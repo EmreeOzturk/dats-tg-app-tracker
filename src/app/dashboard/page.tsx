e32e4dae99331bd424d0069f6896e38c490172d7
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 06:44:26 +0300

    Add LogoutButton component to User Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import UserTable from '@/components/dashboard/user-table';
 import { redirect } from "next/navigation";
 import { verifyAuth } from '@/lib/verifyUser';
+import LogoutButton from '@/components/dashboard/logout-button';
 export default async function DashboardPage() {
     const user = await verifyAuth();
     if (!user) {
@@ -10,7 +11,11 @@ export default async function DashboardPage() {
 
     return (
         <div className="container mx-auto py-10">
-            <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
+           <div className='flex items-center justify-between w-full'>
+           <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
+           <LogoutButton />
+           </div>
+            
             <Suspense fallback={<div>Loading...</div>}>
                 <UserTable />
             </Suspense>

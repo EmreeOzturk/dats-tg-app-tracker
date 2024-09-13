5fab14e1a7a6cf426dc8b6d014341f87204e7c41
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 06:57:29 +0300

    Refactor User Dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,14 +7,12 @@ export default async function DashboardPage() {
     if (!user) {
         redirect("/");
     }
-
     return (
         <div className="container mx-auto py-10">
             <div className='flex items-center justify-between w-full'>
                 <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
                 <LogoutButton />
             </div>
-
             <UserTable />
         </div>
     );

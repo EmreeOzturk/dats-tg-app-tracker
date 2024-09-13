3d925b13b755434e1b20e429550f2490fd55bc26
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 06:57:03 +0300

    Refactor UserTable component and improve User Dashboard page layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import UserTable from '@/components/dashboard/user-table';
 import { redirect } from "next/navigation";
 import { verifyAuth } from '@/lib/verifyUser';
@@ -11,14 +10,12 @@ export default async function DashboardPage() {
 
     return (
         <div className="container mx-auto py-10">
-           <div className='flex items-center justify-between w-full'>
-           <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
-           <LogoutButton />
-           </div>
-            
-            <Suspense fallback={<div>Loading...</div>}>
-                <UserTable />
-            </Suspense>
+            <div className='flex items-center justify-between w-full'>
+                <h1 className="text-3xl font-bold mb-5">User Dashboard</h1>
+                <LogoutButton />
+            </div>
+
+            <UserTable />
         </div>
     );
 }

f4d3887702c304f0db21f83db2481beca545b074
Emre Ozturk <emreeozturk42@gmail.com>
2024-09-13 07:25:13 +0300

    Refactor RootLayout component to remove unnecessary background color class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LoginButton from "@/components/home/login-button";
 
 export default function Homepage() {
   return (
-    <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-screen p-4 ">
+    <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-screen p-4  ">
       <LoginButton />
       <HeroGlobe />
     </div>
